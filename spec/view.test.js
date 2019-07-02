dd9d410ec7d0f425ddd64f83dbf0b98c01bdd8d2
Luke Phyall <shoreditchwarlock@gmail.com>
2019-07-02 15:50:14 +0100

    Adds test to View for edge case - empty List

--- spec/view.test.js
+++ spec/view.test.js
@@ -4,12 +4,18 @@ describe('View', () => {
   const listMock = {
     notes: () => ['Some text', 'Some more text', 'Yet even more text'],
   };
-
+  const emptyListMock = { notes: () => [''] };
   const view = new View(listMock);
+  const anotherView = new View(emptyListMock);
 
   it('returns a string of HTML from the List object', () => {
     const html = '<ul><li>Some text</li><li>Some more text</li><li>Yet even more text</li></ul>';
 
     expect(view.format()).toEqual(html);
+  });
+
+  it('can handle an empty List', () => {
+    const emptyHTML = '<ul><li></li></ul>';
+    expect(anotherView.format()).toEqual(emptyHTML);
   });
 });
